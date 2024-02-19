c807b4b1db9671450f9f779f76138748e13fdc9d
MichaelWhiting <michaelbwhiting2004@gmail.com>
2024-02-19 13:12:10 -0700

    added some queries

--- backend/database/queries.js
+++ backend/database/queries.js
@@ -2,6 +2,11 @@ import { User, Score, Game } from './model.js';
 
 // Some queries to test some things
 
+// 1. Getting all of the scores for the Wordle Game
+const wordleScores = await Score.findAll({ 
+    where: {
+        gameName: "wordle"
+    }
+})
 
-
-// Getting all of the scores for the Wordle Game
+console.log(wordleScores);
